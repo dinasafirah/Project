dcd3ace57505d4c502f4a8fa2215ba393f280193
youlikejelly <49085238+youlikejelly@users.noreply.github.com>
2019-04-04 03:26:52 +0700

    Update Project.c

--- Project.c
+++ Project.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-printJadwal(char **arrJadwal);
+typedef struct {
+    char matkul[6], rombongan[2];
+} praktikum_t;
+
+typedef struct {
+    char asisten1, asisten2;
+} asisten_t;
+typedef struct {
+    int minggu;
+    char hari[10]
+} rule_t;
 
 int main(){
 
   FILE *fileJadwal, *fileAsisten, *fileRule;
   char namaFile[80];
   int kode;
+  praktikum_t arrJadwal[12][5][4];
+  asisten_t arrAsisten[12][5][4];
   scanf("%d", &kode);
   while ((kode != 1) || (kode != 2) || (kode != 3)){
   switch(kode){
@@ -15,14 +27,15 @@ int main(){
         //Membuat proyek baru
         printf("====== Membuat Proyek Baru ======\nMasukkan nama proyek:");
         scanf("%s", namaFile);
-        fileJadwal = fopen(strcat(namaFile, "_rombongan.csv");, "wb");
-        fileAsisten = fopen(strcat(namaFile, "_asisten.csv");, "wb");
+        fileJadwal = fopen(strcat(namaFile, "_rombongan.csv"), "w+");
+        fileAsisten = fopen(strcat(namaFile, "_asisten.csv"), "w+");
+        fileRule = fopen("Schedule_rule.txt", r);
         printf("[Menu Utama]\nPilih Mode:\n1. Tampilkan Schedule\n2. Schedule Manual\n3. Tampilkan Status Assignment Asisten\n");
         printf("4. Assign Asisten Manual\n5. Schedule Otomatis\n6. Assign Otomatis\n7. Rule Checker\n8. Simpan Proyek dan Keluar\n");
         scanf("%d", &kode);
         while (kode != 8){
           while ((kode != 1) || (kode != 2) || (kode != 3) || (kode != 4) || (kode != 5) || (kode != 6) || (kode != 7)){
-             printf("Masukan salah. Masukan pilihan menu ulang);
+             printf("Masukan salah. Masukan pilihan menu ulang");
              printf("[Menu Utama]\nPilih Mode:\n1. Tampilkan Schedule\n2. Schedule Manual\n3. Tampilkan Status Assignment Asisten\n");
              printf("4. Assign Asisten Manual\n5. Schedule Otomatis\n6. Assign Otomatis\n7. Rule Checker\n8. Simpan Proyek dan Keluar\n")
              scanf("%d", &kode);
@@ -33,7 +46,7 @@ int main(){
               break;
            case 2:
               //schedule manual
-              break;         
+              break;
            case 3:
               //Tampilkan Status Assignment Asisten
               break;
@@ -50,14 +63,18 @@ int main(){
               //Rule Checker
              break;
           }
-        }  
-        if kode = 8 {
+        }
+        if (kode) = 8 {
           //Simpan dan keluar
         }
     break;
     case 2:
         //Load proyek dari data
-
+        printf("====== Load Proyek dari Data ======\nMasukkan nama proyek:");
+        scanf("%s", namaFile);
+        fileJadwal = fopen(strcat(namaFile, "_rombongan.csv"), "r+");
+        fileAsisten = fopen(strcat(namaFile, "_asisten.csv"), "r+");
+        fileRule = fopen("Schedule_rule.txt", r);
 
         break;
     case 3:
